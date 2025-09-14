a0d0e1987a5d581a20a1236d7f44d2c8808a3c8d
atomattias <atomattias@github.com>
2025-09-14 19:50:08 +0200

    Clean up repository: Remove deleted files and add remaining untracked files

--- CCN_Mobile_App/lib/models/_index.dart
+++
@@ -1,4 +0,0 @@
-export 'user.dart';
-export 'channel.dart';
-export 'message.dart';
-export 'subscription.dart';
--- CCN_Mobile_App/lib/utils/_index.dart
+++
@@ -1,4 +0,0 @@
-export 'app_router.dart';
-export 'app_theme.dart';
-export 'text_theme.dart';
-export 'http.dart';
